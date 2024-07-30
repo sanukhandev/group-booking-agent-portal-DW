057e45a014f3c1e197a1b963dcf4d0d6b2774fd3
Sanu Khan <sanulgbello@gmail.com>
2024-07-31 03:05:44 +0400

    Ui Imporvements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,36 @@
-import Cards from '../components/Cards';
+import React from 'react';
+import Cards from '../components/Home/Cards';
+import InfoCards from '../components/Home/InfoCards';
 import cardData from '../data/HomeCard';
+import { DashData } from '../data/DashData';
 
-const Home = () => {
+const Home: React.FC = () => {
     return (
-        <div className="flex flex-wrap justify-center gap-4 p-4">
-            {cardData.map((data, index) => (
-                <Cards key={index}
-                    image={data.image}
-                    buttonText={data.buttonText}
-                    buttonLink={data.buttonLink}
-                />
-            ))}
+        <div className="p-6 bg-gray-100 min-h-screen">
+            <div className="flex flex-wrap justify-center gap-6 mb-6">
+                {cardData.map((data, index) => (
+                    <Cards
+                        key={index}
+                        image={data.image}
+                        buttonText={data.buttonText}
+                        buttonLink={data.buttonLink}
+                    />
+                ))}
+            </div>
+            {/*<div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6 text-white">*/}
+            {/*    {DashData.map((data, index) => (*/}
+            {/*        <InfoCards*/}
+            {/*            key={index}*/}
+            {/*            title={data.title}*/}
+            {/*            amount={data.amount}*/}
+            {/*            percentage={data.percentage}*/}
+            {/*            trend={data.trend}*/}
+            {/*            lastWeek={data.lastWeek}*/}
+            {/*            gradientFrom={data.gradientFrom}*/}
+            {/*            gradientTo={data.gradientTo}*/}
+            {/*        />*/}
+            {/*    ))}*/}
+            {/*</div>*/}
         </div>
     );
 }
